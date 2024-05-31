e60891f884050aaab39c333255ca19150224b31a
kingslayerx45 <79833637+kingslayerx45@users.noreply.github.com>
2024-05-31 14:30:40 +0800

    leave request filing

--- src/main/java/com/jomariabejo/motorph/repository/LeaveRequestFilling.java
+++
@@ -1,4 +0,0 @@
-package com.jomariabejo.motorph.repository;
-
-public class LeaveRequestFilling {
-}
---
+++ src/main/java/com/jomariabejo/motorph/repository/LeaveRequestFillingRepository.java
@@ -0,0 +1,13 @@
+package com.jomariabejo.motorph.repository;
+
+import com.jomariabejo.motorph.database.DatabaseConnectionUtility;
+import com.jomariabejo.motorph.entity.LeaveRequest;
+import com.jomariabejo.motorph.utility.TextReader;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class LeaveRequestFillingRepository {
+
+
+}
