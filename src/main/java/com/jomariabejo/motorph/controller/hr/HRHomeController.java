9101c835010fbbe9cb4d80116973cf098c9a95cd
jomari abejo <jomariabejostorage@gmail.com>
2024-07-11 06:37:49 +0800

    update data.sql

--- src/main/java/com/jomariabejo/motorph/controller/hr/HRHomeController.java
+++ src/main/java/com/jomariabejo/motorph/controller/hr/HRHomeController.java
@@ -1,10 +1,14 @@
 package com.jomariabejo.motorph.controller.hr;
 
+import com.jomariabejo.motorph.database.DatabaseConnectionUtility;
+import com.jomariabejo.motorph.entity.Allowance;
 import com.jomariabejo.motorph.entity.Employee;
+import com.jomariabejo.motorph.enums.EmployeeStatus;
+import com.jomariabejo.motorph.service.AllowanceService;
 import com.jomariabejo.motorph.service.EmployeeService;
 import com.jomariabejo.motorph.utility.AlertUtility;
 import com.jomariabejo.motorph.utility.CSVReaderUtility;
-import com.opencsv.CSVReader;
+import com.jomariabejo.motorph.utility.DateUtility;
 import com.opencsv.exceptions.CsvException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -29,8 +33,9 @@ import javafx.stage.StageStyle;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.file.Path;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -60,6 +65,8 @@ public class HRHomeController {
 
     private final EmployeeService employeeService = new EmployeeService();
 
+    private final AllowanceService allowanceService = new AllowanceService();
+
     @FXML
     private void initialize() {
         try {
@@ -68,7 +75,6 @@ public class HRHomeController {
             throw new RuntimeException(e);
         }
     }
-
 
 
     /**
@@ -114,6 +120,7 @@ public class HRHomeController {
             final Button deleteButton = new Button();
 
             final HBox actionsBox = new HBox(editButton, viewButton, deleteButton);
+
             {
                 actionsBox.setAlignment(Pos.CENTER); // Align HBox content to center
                 actionsBox.setSpacing(5); // Set spacing between buttons
@@ -274,7 +281,7 @@ public class HRHomeController {
                 refreshTable();
             }
         } catch (NumberFormatException e) {
-            if(tf_searchField.getText().isEmpty()) {
+            if (tf_searchField.getText().isEmpty()) {
                 refreshTable();
             }
             AlertUtility.showErrorAlert("Invalid employee id", null, "Invalid employee id " + tf_searchField.getText());
@@ -320,29 +327,9 @@ public class HRHomeController {
             String csvPath;
             csvPath = AlertUtility.showPathInputDialog();
             boolean fileExist = checkIfExist(csvPath);
-            boolean columnSizeShouldBe19 = CSVReaderUtility.readAllLines(Path.of(csvPath)).size() == 18;
-
-            ArrayList<String> defaultHeaders = new ArrayList<>();
-
-            defaultHeaders.add("Employee #");
-            defaultHeaders.add("Last Name");
-            defaultHeaders.add("First Name");
-            defaultHeaders.add("Birthday");
-            defaultHeaders.add("Address");
-            defaultHeaders.add("Phone Number");
-            defaultHeaders.add("SSS #");
-            defaultHeaders.add("Philhealth #");
-            defaultHeaders.add("TIN #");
-            defaultHeaders.add("Pag-ibig #");
-            defaultHeaders.add("Status");
-            defaultHeaders.add("Position");
-            defaultHeaders.add("Immediate Supervisor");
-            defaultHeaders.add("Basic Salary");
-            defaultHeaders.add("Rice Subsidy");
-            defaultHeaders.add("Phone Allowance");
-            defaultHeaders.add("Clothing Allowance");
-            defaultHeaders.add("Gross Semi-monthly Rate");
-            defaultHeaders.add("Hourly Rate");
+            boolean columnSizeShouldBe19 = CSVReaderUtility.readHeaders(Path.of(csvPath)).size() == 19;
+            System.out.println("Size sample: " + CSVReaderUtility.readHeaders(Path.of(csvPath)).size());
+            ArrayList<String> defaultHeaders = getStrings();
 
 
             // Normalize and trim headers
@@ -358,15 +345,12 @@ public class HRHomeController {
 
             if (!fileExist) {
                 throw new FileNotFoundException();
-            }
-            else if (!columnSizeShouldBe19) {
+            } else if (!columnSizeShouldBe19) {
                 AlertUtility.showErrorAlert
                         ("Invalid csv path",
                                 "The csv that you have entered has invalid column size",
                                 "Please make sure that you're using the 'multi-employee-registration-template.csv'");
-            }
-
-            else if (!normalizedDefaultHeaders.equals(normalizedCSVHeaders)) {
+            } else if (!normalizedDefaultHeaders.equals(normalizedCSVHeaders)) {
                 AlertUtility.showErrorAlert(
                         "Headers do not match",
                         "Please try again.",
@@ -374,13 +358,131 @@ public class HRHomeController {
             } else {
                 saveEmployees(CSVReaderUtility.readAllLines(Path.of(csvPath)));
             }
-        }
-        catch (IOException | CsvException e) {
-
+        } catch (IOException | CsvException e) {
+            AlertUtility.showErrorAlert(e.getMessage(), "File not found", e.getLocalizedMessage());
         }
     }
 
-    private void saveEmployees(List<String[]> strings) {
+    private static ArrayList<String> getStrings() {
+        ArrayList<String> defaultHeaders = new ArrayList<>();
+        defaultHeaders.add("Employee #");
+        defaultHeaders.add("Last Name");
+        defaultHeaders.add("First Name");
+        defaultHeaders.add("Birthday");
+        defaultHeaders.add("Address");
+        defaultHeaders.add("Phone Number");
+        defaultHeaders.add("SSS #");
+        defaultHeaders.add("Philhealth #");
+        defaultHeaders.add("TIN #");
+        defaultHeaders.add("Pag-ibig #");
+        defaultHeaders.add("Status");
+        defaultHeaders.add("Position");
+        defaultHeaders.add("Immediate Supervisor");
+        defaultHeaders.add("Basic Salary");
+        defaultHeaders.add("Rice Subsidy");
+        defaultHeaders.add("Phone Allowance");
+        defaultHeaders.add("Clothing Allowance");
+        defaultHeaders.add("Gross Semi-monthly Rate");
+        defaultHeaders.add("Hourly Rate");
+        return defaultHeaders;
+    }
+
+    private void saveEmployees(List<String[]> employeeList) {
+        employeeList.remove(0); // remove headers
+        try {
+            for (String[] data : employeeList) {
+                Employee newEmployee = mapToEmployee(data);
+                Allowance allowance = mapToAllowance(data);
+                employeeService.saveEmployee(newEmployee);
+                allowanceService.createAllowance(allowance);
+            }
+            AlertUtility.showInformation("Success", "Employee saved successfully", String.valueOf(employeeList.size()) + " employees added." );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Allowance mapToAllowance(String[] data) {
+        Allowance allowance = new Allowance();
+        allowance.setEmployeeID(Integer.parseInt(data[0]));
+        allowance.setRiceAllowance(Integer.parseInt(data[14]));
+        allowance.setClothingAllowance(Integer.parseInt(data[15]));
+        allowance.setPhoneAllowance(Integer.parseInt(data[16]));
+        allowance.setTotalAmount(allowance.getRiceAllowance() + allowance.getClothingAllowance() + allowance.getPhoneAllowance());
+        allowance.setDateCreated(DateUtility.getDate());
+        return allowance;
+    }
+
+    private Employee mapToEmployee(String[] data) {
+        Employee employee = new Employee();
+        employee.setEmployeeId(Integer.parseInt(data[0]));
+        employee.setLastName(data[1]);
+        employee.setFirstName(data[2]);
+        employee.setBirthday(switchToYYYYMMDD(data[3]));
+        employee.setAddress(data[4]);
+        employee.setContactNumber(data[5]);
+        employee.setSss(data[6]);
+        employee.setPhilhealth(data[7]);
+        employee.setTin(data[8]);
+        employee.setPagibig(data[9]);
+        employee.setStatus(EmployeeStatus.valueOf(data[10]));
+        employee.setPositionId(switchPositionNameToPositionId(data[11]));
+        employee.setDeptId(switchDepartmentNameToDepartmentId(data[12]));
+        employee.setSupervisor(data[12]);
+        employee.setBasicSalary(BigDecimal.valueOf(Long.parseLong(data[13])));
+        employee.setDateHired(DateUtility.getDate());
+        employee.setGrossSemiMonthlyRate(BigDecimal.valueOf(Float.parseFloat(data[17])));
+        employee.setHourlyRate(BigDecimal.valueOf(Float.parseFloat(data[18])));
+        return employee;
+    }
+
+    private int switchDepartmentNameToDepartmentId(String departmentName) {
+        String query = "SELECT position.position_id FROM position WHERE position.name = ?";
+
+        try (Connection connection = DatabaseConnectionUtility.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(query)
+        ) {
+            pstmt.setString(1, departmentName);
+
+            ResultSet resultSet = pstmt.executeQuery();
+
+            if (resultSet.next()) {
+                return resultSet.getInt(1);
+            } else return 0; // not found
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int switchPositionNameToPositionId(String positionName) {
+        String query = "SELECT position.position_id FROM position WHERE position.name = ?";
+
+        try (Connection connection = DatabaseConnectionUtility.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(query)
+        ) {
+            pstmt.setString(1, positionName);
+
+            ResultSet resultSet = pstmt.executeQuery();
+
+            if (resultSet.next()) {
+                return resultSet.getInt(1);
+            } else return 0; // not found
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Date switchToYYYYMMDD(String datum) {
+        String[] date = datum.split("/");
+        short year = Short.parseShort(date[2]);
+        byte month = Byte.parseByte(date[0]);
+        byte day = Byte.parseByte(date[1]);
+
+        // Correct way to format the date as "YYYY-MM-DD"
+        String formattedDate = String.format("%04d-%02d-%02d", year, month, day);
+
+        // Return the java.sql.Date object
+        return Date.valueOf(formattedDate);
     }
 
     private static List<String> normalizeAndTrimHeaders(List<String> headers) {
