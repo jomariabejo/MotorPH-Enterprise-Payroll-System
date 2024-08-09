a78fef51759eb7a61f5f3e2a3767cfdfc082c560
Jomari S. Abejo <70135786+jomariabejo@users.noreply.github.com>
2024-08-09 09:01:03 +0800

    optimize code

--- src/main/java/com/jomariabejo/motorph/controller/role/employee/LeaveBalanceController.java
+++ src/main/java/com/jomariabejo/motorph/controller/role/employee/LeaveBalanceController.java
@@ -28,59 +28,33 @@ public class LeaveBalanceController {
         // Constructor
     }
 
-    public void populatePieChartWithSampleData() {
-        try {
-            PieChart.Data[] sampleData = new PieChart.Data[]{
-                    new PieChart.Data("Sick Leave", 30),
-                    new PieChart.Data("Vacation Leave", 20),
-                    new PieChart.Data("Personal Leave", 10),
-                    new PieChart.Data("Unpaid Leave", 15)
-            };
-
-            pieChartLeaveBalance.setData(FXCollections.observableArrayList(sampleData));
-
-            for (PieChart.Data data : sampleData) {
-                data.setName(data.getName() + " (" + (int) data.getPieValue() + ")");
-            }
-
-            pieChartLeaveBalance.setTitle("Remaining Leave Balance");
-        } catch (Exception e) {
-            e.printStackTrace(); // Print the stack trace for debugging
-        }
-    }
-
     public void populatePieChart() {
-        // Assuming you have a method to fetch leave balance list
         Employee employee = this.getEmployeeRoleNavigationController().getMainViewController().getEmployee();
         Optional<List<LeaveBalance>> leaveBalanceOpt = this.getEmployeeRoleNavigationController().getMainViewController().getLeaveBalanceService().fetchEmployeeRemainingLeaveBalances(employee);
 
         if (leaveBalanceOpt.isPresent()) {
             try {
                 List<LeaveBalance> leaveBalanceList = leaveBalanceOpt.get();
-
                 ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
 
                 for (LeaveBalance leaveBalance : leaveBalanceList) {
-                    PieChart.Data data = new PieChart.Data(leaveBalance.getLeaveTypeID().getLeaveTypeName(), leaveBalance.getBalance());
+                    String leaveTypeName = leaveBalance.getLeaveTypeID().getLeaveTypeName();
+                    double balance = leaveBalance.getBalance();
+                    String formattedName = String.format("%s (%d)", leaveTypeName, (int) balance);
+
+                    PieChart.Data data = new PieChart.Data(formattedName, balance);
                     pieChartData.add(data);
                 }
 
                 pieChartLeaveBalance.setData(pieChartData);
-
-                for (PieChart.Data data : pieChartData) {
-                    String formattedName = data.getName() + " (" + (int) data.getPieValue() + ")";
-                    data.setName(formattedName);
-                }
-
                 pieChartLeaveBalance.setTitle("Remaining Leave Balance");
 
             } catch (Exception e) {
-                e.printStackTrace();
+                System.err.println("Error populating pie chart: " + e.getMessage());
+                e.printStackTrace(); // Consider logging this exception to a file or monitoring system
             }
         } else {
             System.out.println("No leave balance data available.");
         }
     }
-
-
 }
